f0f97728a1c2f646138b862d065d0abd9186563c
dmila012 <dmila012@fiu.edu>
2022-11-22 16:01:25 -0500

    Added Forum Tab and View Controller

---
+++ HelpDev/ForumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForumViewController.swift
+//  HelpDev
+//
+//  Created by Daniel Milanes  on 11/22/22.
+//
+
+import UIKit
+
+class ForumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
