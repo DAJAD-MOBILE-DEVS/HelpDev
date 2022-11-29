69edaafdba627060d431dc7ceddeee010044e576
David Delgado Lopez <ddelg112@fiu.edu>
2022-11-29 14:30:34 -0500

    Java tab populated

--- HelpDev/JavaViewController.swift
+++ HelpDev/JavaViewController.swift
@@ -4,15 +4,21 @@
 //
 //  Created by David Delgado on 11/15/22.
 //
-
+import youtube_ios_player_helper
 import UIKit
 
 class JavaViewController: UIViewController {
 
+    
+    @IBOutlet var playerView: YTPlayerView!
+    @IBOutlet var playerView2: YTPlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        playerView.load(withVideoId: "so1iUWaLmKA&t=6s&ab_channel=BroCode", playerVars: ["playsinline": 1])
+        
+        playerView2.load(withVideoId: "Rn16ugyorX0&ab_channel=BroCode", playerVars: ["playsinline": 1])
     }
     
 
