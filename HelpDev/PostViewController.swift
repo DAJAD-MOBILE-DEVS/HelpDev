29b6c12c01f3927c689ccdfad4982df5620191bc
dmila012 <dmila012@fiu.edu>
2022-11-29 15:03:47 -0500

    Expanded features on Forum, can now reply to posts.

--- HelpDev/PostViewController.swift
+++ HelpDev/PostViewController.swift
@@ -71,7 +71,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         let image = info[.editedImage] as! UIImage
         
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af.imageScaled(to: size)
+        let scaledImage = image.af.imageAspectScaled(toFit: size)
         
         imageView.image = scaledImage
         
